6afb1d290b72eb8500ece4ef86cfa51758441803
hidekuro <hidekuro@gmail.com>
2017-02-07 23:26:22 +0900

    modify to ordered starting

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ set -e
 cd $(dirname $0)
 
 for i in 0 1 2; do
-  nohup docker-machine start manager-${i} > /dev/null 2>&1 &
+  docker-machine start manager-${i}
   for n in 0 1; do
-    nohup docker-machine start worker-${i}-${n} > /dev/null 2>&1 &
+    docker-machine start worker-${i}-${n}
   done
 done
